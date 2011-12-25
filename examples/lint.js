ed6fab0dfe8edc723791baa36271132cf126d214
Mark Constable <markc@renta.net>
2011-12-25 15:09:32 +1000

    Added 2 examples and modified CMakeLists.txt for the new src/ dir

---
+++ examples/lint.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env silkjs
+// lint.js 20111225 (C) markc@renta.net (AGPLv3)
+// A simple script to test Javascript according to http://www.jslint.com/
+// Usage: [silkjs] lint.js file_to_check.js (ie; silkjs lint.js lint.js)
+
+/*global fs: false, include: false, JSLINT: false */
+
+'use strict';
+
+var main = function () {
+  var argv = Array.prototype.slice.call(arguments),
+    argc = argv.length,
+    str = "",
+    res = "";
+
+  if (fs.exists("jslint.js")) {
+    include("jslint.js");
+  } else {
+    console.log("wget https://raw.github.com/douglascrockford/JSLint/master/jslint.js");
+    process.exit();
+  }
+
+  if (argc) {
+    str = fs.readFile(argv[0]);
+    res = JSLINT(str.replace(/[\w\W]+?\n+?/, ''), {
+      node: true,
+      nomen: false,
+      indent: 2
+    });
+
+    if (res) {
+      console.log(argv[0] + " passed the lint test");
+    } else {
+      console.log(argv[0] + " did NOT pass the lint test");
+    }
+  } else {
+    console.log("Usage: [silkjs] lint.js file_to_check.js");
+  }
+};
