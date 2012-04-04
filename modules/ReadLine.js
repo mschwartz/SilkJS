6ca1f2d9e54d00e59b5ad9e2a75789a992f2ee28
Michael Schwartz <mschwartz@pint.com>
2012-04-04 10:28:56 -0700

    Added comments.

--- modules/ReadLine.js
+++ modules/ReadLine.js
@@ -14,24 +14,67 @@
  * while ((var text = stdin.gets())) {
  *      console.log(text);
  * }
- * in.close();
+ * stdin.close();
  * ```
  */
 
 var editline = require('builtin/editline');
 
+/** 
+ * @constructor ReadLine
+ * 
+ * ### Synopsis:
+ * 
+ * var stdin = new ReadLine(program_name);
+ * 
+ * @param {string} program_name - name of program (see man editline).
+ */
 function ReadLine(prog) {
     prog = prog || 'SilkJS';
     this.handle = editline.init(prog);
     editline.prompt(this.handle, '> ');
 }
 ReadLine.prototype.extend({
+    /**
+     * @function stdin.prompt
+     * 
+     * ### Synopsis
+     * 
+     * stdin.prompt(str);
+     * 
+     * Set the prompt string.  The prompt is displayed on lines the user is to type in.  The default prompt is '> '.
+     * 
+     * @param {string} str - new prompt string.
+     * 
+     */
     prompt: function(txt) {
         editline.prompt(this.handle, txt);
     },
+    
+    /**
+     * @function stdin.gets
+     * 
+     * ### Synopsis
+     * 
+     * var line = stdin.gets();
+     * 
+     * Read a line from stdin with prompt.
+     * 
+     * @return {string} line - text that user entered, or false if EOF (e.g. user hit ^D).
+     */
     gets: function() {
         return editline.gets(this.handle);
     },
+    
+    /**
+     * @function stdin.close
+     * 
+     * ### Synopsis
+     * 
+     * stdin.close();
+     * 
+     * Close ReadLine instance and free any resources used.
+     */
     close: function() {
         editline.end(this.handle);
     }
